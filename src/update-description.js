7c467ea9af9a8c85ef8bf9c283bd772fd05d3e9a
Michael Henderson <mhenderson@keyfactor.com>
2023-07-13 16:17:44 -0700

    updated everything

--- src/update-description.js
+++ src/update-description.js
@@ -8,61 +8,50 @@ const token = core.getInput('repo-token');
 
 const github = new Octokit({ auth: token });
 const { owner, repo } = context.repo;
-console.log(`owner: ${owner}, repo: ${repo}`)
-
 async function getRepoDescription(owner, repo) {
   console.log(`Getting description for Repo: ${repo}`);
-  const response = await github.request("GET /repos/{owner}/{repo}", {
+  const { data } = await octokit.repos.get({
     owner,
     repo
   });
-  const { description } = response.data;
-  console.log(response)
-  console.log(`description: ${description}`)
+
+  description = data.description
+  console.log(`Repo Description: ${description}`)
   return description;
 }
+
 function getManifestDescription() {
-  console.log(`Getting description from manifest: ${jsonPath}`);
+  console.log(`Getting description from manifest file: ${jsonPath}`);
   try {
     const repoJSONProps = JSON.parse(fs.readFileSync(jsonPath));
     const jsonDescription = repoJSONProps.description;
-    console.log(`jsonDescription: ${jsonDescription}`)
     return jsonDescription;
   } catch (e) {
+    console.log(e.message)
   }
 }
-  async function updateRepoDescription(owner, repo) {
-    try {
-      const description = getManifestDescription()
-      const response = await github.request('PATCH /repos/{owner}/{repo}', {
-        owner,
-        repo,
-        description
-      });
 
-    } catch (e) {
-      console.log(e.message)
-    }
-  }
+async function updateRepoDescription(owner, repo, description) {
+  try {
+    const response = await octokit.request('PATCH /repos/{owner}/{repo}', {
+      owner,
+      repo,
+      description
+    });
 
-  function checkIfEmptyDescription(description) {
-    console.log(`Checking if the description has been defined`)
-    console.log(`description: ${description}`)
-    const isEmpty = (description == null)
-    console.log(`isEmpty: ${isEmpty}`)
-    return isEmpty;
+  } catch (e) {
+    console.log(e.message)
   }
-async function checkAndUpdateDescription(owner, repo) {
-    console.log(`Attempting to update description for ${repo}`)
-    try {
-      const repoDescription = getRepoDescription(owner, repo);
-      if (!checkIfEmptyDescription(repoDescription)) {
-        console.log(`Update ${repo} description: ${repoDescription}`);
-        await updateRepoDescription(owner, repo);
-        }
-    } catch (e) {
-      console.log(e.message)
-    }
-  }
+}
+function main() {
+  jsonDescription = getManifestDescription()
+  repoDescription = getRepoDescription(owner, repo)
+    .then((repoDescription) => {
+      if (repoDescription == null) {
+        console.log(`Updating description to ${jsonDescription}`)
+        updateRepoDescription(owner, repo, jsonDescription)
+      }
+    })
+}
 
-checkAndUpdateDescription(owner, repo)
+main()
